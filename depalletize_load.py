8402550c503a427ab3b9ff7b11199099bb03dcd7
Jessica Tingley <jtingley2003@gmail.com>
2025-04-04 14:37:34 -0400

    Update for new bearing tray

--- depalletize_load.py
+++ depalletize_load.py
@@ -22,13 +22,13 @@ entry_flag = 0
 flip_flag = 0
 motion_time = 0
 
-BEARING_APPROACH = [-57.44, -142.73, -90.89, 48.27, 15.04, -85.94]
-BEARING_CONTACT = [-63.83, -143.65, -92.02, 54.12, 20.74, -90.06]
-BEARING_LEAVE = [-62.74, -125.31, -94.15, 39.45, 19.04, -89.97]
+BEARING_APPROACH = [-74.24, -141.68, -106.94, 72.65, 28.82, -94.27]
+BEARING_CONTACT = [-79.18, -141.68, -106.94, 72.61, 34.51, -94.27]
+BEARING_LEAVE = [-79.18, -138.49, -106.94, 72.61, 34.51, -98.73]
 
-FLIPPED_BEARING_APPROACH = [-64.14, -157.24, -58.14, 37.36, -67.16, -273.06]
-FLIPPED_BEARING_CONTACT = [-68.61, -157.20, -58.14, 38.39, -64.44, -272.70]
-FLIPPED_BEARING_LEAVE = [-68.86, -150.41, -58.14, 32.05, -64.60, -272.57]
+FLIPPED_BEARING_APPROACH = [-76.95, -149.72, -81.01, 48.41, -54.55, -268.35]
+FLIPPED_BEARING_CONTACT = [-80.43, -149.72, -81.01, 48.41, -51.23, -268.35]
+FLIPPED_BEARING_LEAVE = [-80.43, -144.30, -81.01, 48.41, -48.0, -268.35]
 
 LOAD = [-58.10, -101.58, -111.03, -97.66, -66.67, -200.53]
 FLIPPED_LOAD = [-62.64, -108.24, -115.58, -7.20, 72.80, -164.01]
@@ -64,23 +64,14 @@ def state_machine(robot: RDK.Item, num_unloads: int):
     global entry_flag, state, motion_time, flip_flag
 
     # Determine joint offsets for de-palletization
-    offset_x = num_unloads % 2
-    offset_y = (num_unloads // 3) % 13
+    offset = num_unloads % 6
 
     # Flip end effector every other unload
-    if offset_x:
-        bearing_offset_x = [0 * offset_x, -6.67 * offset_x, 15.14 * offset_x, -10.84 * offset_x, 1.19 * offset_x,
-                            0.78 * offset_x]
-        bearing_offset_y = [-2.72 * offset_y, -0.16 * offset_y, 0 * offset_y, 0 * offset_y, 1.78 * offset_y,
-                            -0.20 * offset_y]
+    if num_unloads % 2:
+        bearing_offset = [-2.21 * offset, 0 * offset, 0 * offset, 0 * offset, 1.73 * offset, 0.35 * offset]
         state = States.FLIP_VACUUM if not flip_flag else state
     else:
-        bearing_offset_x = [-2.35 * offset_x, -0.71 * offset_x, 5.72 * offset_x, 9.25 * offset_x, 1.47 * offset_x,
-                            7.26 * offset_x]
-        bearing_offset_y = [-2.72 * offset_y, -0.16 * offset_y, 0 * offset_y, 0 * offset_y, 1.78 * offset_y,
-                            -0.20 * offset_y]
-
-    bearing_offset = [x + y for x, y in zip(bearing_offset_x, bearing_offset_y)]
+        bearing_offset = [-2.55 * offset, 0 * offset, -0.39 * offset, 0.13 * offset, 3.23 * offset, -0.07 * offset]
 
     match state:
         case States.FLIP_VACUUM:
@@ -88,41 +79,43 @@ def state_machine(robot: RDK.Item, num_unloads: int):
                 flip_flag = 1
                 non_blocking_move(robot, [10, 0, 0, 0, -90, -180], BEARING_LEAVE)
 
-            if (time.perf_counter() - motion_time) >= 10:
+            if not robot.Busy():
                 state = States.APPROACH_BEARING
                 entry_flag = 0
 
         case States.APPROACH_BEARING:
             if not entry_flag:
-                if offset_x:
+                if flip_flag:
                     non_blocking_move(robot, bearing_offset, FLIPPED_BEARING_APPROACH)
                 else:
                     non_blocking_move(robot, bearing_offset, BEARING_APPROACH)
 
-            if (time.perf_counter() - motion_time) >= 10:
+            if not robot.Busy():
                 state = States.GRIP_BEARING
                 entry_flag = 0
 
         case States.GRIP_BEARING:
-            if entry_flag == 0:
-                if offset_x:
+            if not entry_flag:
+                if flip_flag:
                     non_blocking_move(robot, bearing_offset, FLIPPED_BEARING_CONTACT)
                 else:
                     non_blocking_move(robot, bearing_offset, BEARING_CONTACT)
 
-            if (time.perf_counter() - motion_time) >= 5:
-                RDK.setDIO(robot, offset_x, 1)
-                state = States.LIFT_BEARING
-                entry_flag = 0
+            if not robot.Busy():
+                robot.setDO(io_value=0, io_var=1)  # First, set D_O0 to low (activate vacuum)
+                robot.setDO(io_value=0, io_var=0)
+                if (time.perf_counter() - motion_time) >= 3:
+                    state = States.LIFT_BEARING
+                    entry_flag = 0
 
         case States.LIFT_BEARING:
             if not entry_flag:
-                if offset_x:
+                if flip_flag:
                     non_blocking_move(robot, bearing_offset, FLIPPED_BEARING_LEAVE)
                 else:
                     non_blocking_move(robot, bearing_offset, BEARING_LEAVE)
 
-            if (time.perf_counter() - motion_time) >= 5:
+            if not robot.Busy():
                 state = States.APPROACH_LOAD if flip_flag else States.APPROACH_BEARING
                 num_unloads += 1
                 entry_flag = 0
@@ -132,7 +125,7 @@ def state_machine(robot: RDK.Item, num_unloads: int):
             if not entry_flag:
                 non_blocking_move(robot, [0, 0, 0, 0, 0, 0], LOAD)
 
-            if (time.perf_counter() - motion_time) >= 5:
+            if not robot.Busy():
                 state = States.APPROACH_BEARING
                 entry_flag = 0
 
